d54a6c8609c5e8fba6962fb834bbea4eb86372b7
Sanjay Utchula <sanjayutchula09@gmail.com>
2025-08-17 10:29:37 +0530

    Implemented Dyanmic json files generation according to Round

--- CSIR1/QUESTIONS/round_questions.py
+++ CSIR1/QUESTIONS/round_questions.py
@@ -8,11 +8,9 @@ CURRENT_QUESTIONS_DIR = os.path.join(os.path.dirname(__file__), 'CURRENT')
 
 def copy_round_files(round_name: str) :
     source_dir = os.path.join(BASE_DATA_DIR, round_name)
-    print(f"Clearing existing files in {CURRENT_QUESTIONS_DIR}")
     for filename in os.listdir(CURRENT_QUESTIONS_DIR):
         file_path = os.path.join(CURRENT_QUESTIONS_DIR, filename)
         os.unlink(file_path)
-    print(f"Copying new JSON files from {source_dir} to {CURRENT_QUESTIONS_DIR}")
     copied_count = 0
     for filename in os.listdir(source_dir):
         if filename.endswith('.json'):
